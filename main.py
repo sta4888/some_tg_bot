fe1f59c9dcfb6f4c3a0b28ca6938123db360d625
ASUS ROG <talonin92@mail.ru>
2024-09-16 09:24:04 +0500

    v0.1

---
+++ main.py
@@ -0,0 +1,29 @@
+import telebot
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, User
+
+# Инициализация бота
+API_TOKEN = "YOUR_BOT_API_KEY"
+bot = telebot.TeleBot(API_TOKEN)
+
+# Настройка подключения к базе данных PostgreSQL через SQLAlchemy
+DATABASE_URL = "postgresql://botuser:@localhost/botdatabase"
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Простой обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Добро пожаловать в нашего бота.")
+
+    # Пример сохранения пользователя в базу данных
+    user = User(telegram_id=message.from_user.id, username=message.from_user.username)
+    session.add(user)
+    session.commit()
+
+
+if __name__ == "__main__":
+    bot.polling()
---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True)
+    username = Column(String(100))
+
