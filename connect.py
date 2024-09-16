8a745ede5a527d57ead80ef257f9a8df5239cee0
User <talonin92@mail.ru>
2024-09-16 11:47:34 +0500

    v0.10

--- connect.py
+++ connect.py
@@ -2,7 +2,10 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 DATABASE_URL = os.environ.get('DB_URL')
 
 # Настройка подключения к базе данных PostgreSQL через SQLAlchemy
