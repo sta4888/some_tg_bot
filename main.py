523b9198ff3477f6fb3f606ea8c00d0f129183b1
User <talonin92@mail.ru>
2024-09-16 13:38:36 +0500

    v0.12

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ def send_welcome(message):
     bot.reply_to(message, "Привет! Добро пожаловать в нашего бота.")
 
     # Пример сохранения пользователя в базу данных
-    user = User(telegram_id=message.from_user.id, username=message.from_user.username)
-    session.add(user)
-    session.commit()
+    user = session.query(User).filter_by(telegram_id=message.from_user.id).first()
+
+    # Если пользователь не найден, создайте нового
+    if user is None:
+        user = User(telegram_id=message.from_user.id, username=message.from_user.username)
+        session.add(user)
 
 
 if __name__ == "__main__":
