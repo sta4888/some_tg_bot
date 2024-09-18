49452d8c62c36f75d8ceda9667983860cd245b63
User <talonin92@mail.ru>
2024-09-18 11:45:13 +0500

    book v0.8 дата выезда

--- main.py
+++ main.py
@@ -22,30 +22,48 @@ def start(message):
 
 def ask_city(message):
     user_data[message.chat.id]['city'] = message.text
-    calendar, step = DetailedTelegramCalendar().build()
+    calendar, step = DetailedTelegramCalendar(min_date=datetime.date.today()).build()
     bot.send_message(message.chat.id, f"Выберите дату заезда:", reply_markup=calendar)
 
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
 def cal(c):
-    result, key, step = DetailedTelegramCalendar().process(c.data)
+    result, key, step = DetailedTelegramCalendar(min_date=datetime.date.today()).process(c.data)
     if not result and key:
-        bot.edit_message_text(f"Select {LSTEP[step]}",
+        bot.edit_message_text(f"Выберите дату {LSTEP[step]}",
                               c.message.chat.id,
                               c.message.message_id,
                               reply_markup=key)
     elif result:
         user_data[c.message.chat.id]['start_date'] = result.strftime('%Y-%m-%d')
-        end_date = result + datetime.timedelta(days=1)
-        user_data[c.message.chat.id]['end_date'] = end_date.strftime('%Y-%m-%d')
 
         bot.edit_message_text(f"Вы выбрали дату заезда: {user_data[c.message.chat.id]['start_date']}.\n"
-                              f"Предлагаем дату выезда: {user_data[c.message.chat.id]['end_date']}.",
+                              f"Введите дату выезда (формат: YYYY-MM-DD):",
                               c.message.chat.id,
                               c.message.message_id)
 
-        bot.send_message(c.message.chat.id, "Сколько спальных мест вам нужно?")
-        bot.register_next_step_handler(c.message, ask_bedrooms)
+        bot.register_next_step_handler(c.message, ask_end_date)
+
+
+def ask_end_date(message):
+    try:
+        end_date = datetime.datetime.strptime(message.text, '%Y-%m-%d')
+        start_date = datetime.datetime.strptime(user_data[message.chat.id]['start_date'], '%Y-%m-%d')
+
+        if end_date <= start_date:
+            raise ValueError
+
+        user_data[message.chat.id]['end_date'] = end_date.strftime('%Y-%m-%d')
+
+        bot.send_message(message.chat.id,
+                         f"Вы выбрали даты:\nЗаезд: {user_data[message.chat.id]['start_date']}\nВыезд: {user_data[message.chat.id]['end_date']}.")
+        bot.send_message(message.chat.id, "Сколько спальных мест вам нужно?")
+        bot.register_next_step_handler(message, ask_bedrooms)
+
+    except ValueError:
+        bot.send_message(message.chat.id,
+                         "Неверный формат даты или дата выезда должна быть позже даты заезда. Пожалуйста, введите дату в формате YYYY-MM-DD.")
+        bot.register_next_step_handler(message, ask_end_date)
 
 
 def ask_bedrooms(message):
