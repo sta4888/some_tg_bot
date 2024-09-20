26bf748c86b17e8fa1caf6b9d5e03a603a03fe82
User <talonin92@mail.ru>
2024-09-20 12:31:53 +0500

    v0.47

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 from connect import session
 from models import User, Offer
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 from service import parse_and_save_offer
@@ -10,9 +9,6 @@ load_dotenv()
 
 API_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(API_TOKEN)
-
-# Временное хранилище данных, пока пользователь вводит ссылки
-user_offer_data = {}
 
 # Словарь для хранения состояния пользователей
 user_states = {}
@@ -23,10 +19,8 @@ user_states = {}
 def send_welcome(message):
     bot.reply_to(message, "Привет! Добро пожаловать в нашего бота.")
 
-    # Поиск пользователя по telegram_id
     user = session.query(User).filter_by(telegram_id=message.from_user.id).first()
 
-    # Если пользователь не зарегистрирован, добавляем его
     if user is None:
         user = User(
             telegram_id=message.from_user.id,
@@ -37,8 +31,7 @@ def send_welcome(message):
         session.add(user)
         session.commit()
 
-    # Приветствие для хостов
-    if user.is_client:  # Или используйте другой флаг для хоста
+    if user.is_client:
         bot.reply_to(message, "Привет! Вы зарегистрированы как пользователь.")
     else:
         bot.reply_to(message, "Добро пожаловать, хост! Пожалуйста, загрузите XML-файл.")
@@ -57,11 +50,24 @@ def handle_document(message):
             xml_data = downloaded_file.decode('utf-8')
             internal_ids = parse_and_save_offer(xml_data, bot, message)
 
-            # Запросим у пользователя ссылки по порядку
-            if internal_ids:
+            # Проверяем, какие internal_id уже существуют
+            existing_ids = {offer.internal_id for offer in
+                            session.query(Offer).filter(Offer.internal_id.in_(internal_ids)).all()}
+
+            # Запросим у пользователя, хочет ли он обновить данные
+            if existing_ids:
+                user_states[message.from_user.id] = {
+                    'internal_ids': list(existing_ids),
+                    'current_index': 0,
+                    'update_existing': True
+                }
+                bot.reply_to(message,
+                             f"Существуют данные для internal_id: {', '.join(existing_ids)}. Вы хотите их обновить? (да/нет)")
+            else:
+                # Если нет существующих записей, запрашиваем ссылки
                 user_states[message.from_user.id] = {
                     'internal_ids': internal_ids,
-                    'current_index': 0  # Индекс текущего internal_id
+                    'current_index': 0
                 }
                 bot.reply_to(message, f"Пожалуйста, введите URL для предложения с internal_id: {internal_ids[0]}")
 
@@ -71,18 +77,57 @@ def handle_document(message):
         bot.reply_to(message, "Этот файл не является XML.")
 
 
-# Обработка текстовых сообщений от пользователей
-@bot.message_handler(func=lambda message: message.from_user.id in user_states)
+# Обработка ответа на запрос обновления
+@bot.message_handler(
+    func=lambda message: message.from_user.id in user_states and 'update_existing' in user_states[message.from_user.id])
+def handle_update_confirmation(message):
+    user_id = message.from_user.id
+    user_state = user_states[user_id]
+
+    if message.text.strip().lower() == 'да':
+        # Обновляем данные для существующих internal_id
+        internal_ids = user_state['internal_ids']
+        current_index = user_state['current_index']
+        internal_id = internal_ids[current_index]
+        offer = session.query(Offer).filter_by(internal_id=internal_id).first()
+
+        if offer:
+            # Здесь нужно обновить данные предложения
+            # Например:
+            # offer.url_to = 'новый URL'  # Сначала получите новый URL от пользователя
+            session.commit()
+            bot.reply_to(message, f"Данные для internal_id {internal_id} обновлены.")
+
+            # Переход к следующему internal_id
+            current_index += 1
+            user_state['current_index'] = current_index
+
+            if current_index < len(internal_ids):
+                next_internal_id = internal_ids[current_index]
+                bot.reply_to(message,
+                             f"Обновите данные для internal_id: {next_internal_id}. Пожалуйста, введите новый URL.")
+            else:
+                del user_states[user_id]  # Удаляем пользователя из состояния
+                bot.reply_to(message, "Все данные успешно обновлены.")
+        else:
+            bot.reply_to(message, f"Предложение с internal_id {internal_id} не найдено.")
+    else:
+        # Если пользователь не хочет обновлять, просто удаляем состояние
+        del user_states[user_id]
+        bot.reply_to(message, "Обновление данных отменено.")
+
+
+# Обработка текстовых сообщений от пользователей для ввода URL
+@bot.message_handler(func=lambda message: message.from_user.id in user_states and 'update_existing' not in user_states[
+    message.from_user.id])
 def handle_url_input(message):
     user_id = message.from_user.id
     user_state = user_states[user_id]
 
-    # Получаем ссылку от пользователя
     url_to = message.text.strip()
     internal_ids = user_state['internal_ids']
     current_index = user_state['current_index']
 
-    # Получаем текущий internal_id
     internal_id = internal_ids[current_index]
     offer = session.query(Offer).filter_by(internal_id=internal_id).first()
 
@@ -91,16 +136,14 @@ def handle_url_input(message):
         session.commit()
         bot.reply_to(message, f"Ссылка для internal_id {internal_id} обновлена на: {url_to}")
 
-        # Переходим к следующему internal_id
         current_index += 1
         user_state['current_index'] = current_index
 
-        # Проверяем, остались ли еще internal_ids
         if current_index < len(internal_ids):
             next_internal_id = internal_ids[current_index]
             bot.reply_to(message, f"Пожалуйста, введите URL для предложения с internal_id: {next_internal_id}")
         else:
-            del user_states[user_id]  # Удаляем пользователя из состояния
+            del user_states[user_id]
             bot.reply_to(message, "Все ссылки успешно обновлены.")
     else:
         bot.reply_to(message, f"Предложение с internal_id {internal_id} не найдено.")
