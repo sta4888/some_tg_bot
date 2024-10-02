6ec948359b5ad230656fe86c035dc2e5b5c622b0
User <talonin92@mail.ru>
2024-10-02 15:48:26 +0500

    book v0.96

--- celery.py
+++ celery.py
@@ -1,11 +1,12 @@
 from celery import Celery
 import os
 
-app = Celery('telegram_bot')
-
 # Настройки Celery
-app.conf.broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-app.conf.result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+
+app = Celery('telegram_bot', broker_pool_limit=1, broker=broker_url,
+             result_backend=result_backend)
 
 # Пример расписания задач (если нужно)
 app.conf.beat_schedule = {
