b333e536634b77f52d987b017ff0eb214ad8a557
User <talonin92@mail.ru>
2024-10-02 16:13:32 +0500

    book v0.99

--- celery.py
+++ celery.py
@@ -3,7 +3,7 @@ import os
 import os
 from loguru import logger
 
-broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379')
 result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 logger.info(f'Broker URL: {broker_url}')
