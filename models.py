fd481ffe30427f2386593a36450919a330ac8d65
User <talonin92@mail.ru>
2024-10-05 16:23:06 +0500

    main v0.167

--- models.py
+++ models.py
@@ -21,6 +21,7 @@ class User(Base):
     is_client = Column(Boolean, nullable=False, default=True)
     referer_id = Column(Integer, ForeignKey('users.id'), nullable=True)
     referer = relationship('User', remote_side=[id], backref='referred_users')
+    employer = relationship('User', remote_side=[id], backref='referred_users')
 
     role_id = Column(Integer, ForeignKey('role.id'), nullable=False)  # Добавляем роль
     role = relationship("Role", back_populates="users")  # Связь с моделью Role
